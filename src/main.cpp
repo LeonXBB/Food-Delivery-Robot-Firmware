e0e9a2aed981556af1ba2adbd33086de90c2ae79
LeonXBB <lbarenbojm@gmail.com>
2024-07-25 12:44:14 +0300

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,50 @@
 #include <Arduino.h>
 #include "crsf.h"
 
-#define RXD2 16
-#define TXD2 17
 #define SBUS_BUFFER_SIZE 25
 uint8_t _rcs_buf[25] {};
+uint16_t _old_rc_values[RC_INPUT_MAX_CHANNELS] {};
 uint16_t _raw_rc_values[RC_INPUT_MAX_CHANNELS] {};
 uint16_t _raw_rc_count{};
 
-int aileronsPin = 12;
-int elevatorPin = 13;
-int throttlePin = 14;
-int rudderPin = 15;
 
-int aileronsPWMChannel = 1;
-int elevatorPWMChannel = 2;
-int throttlePWMChannel = 3;
-int rudderPWMChannel = 4;
+#define RX_PIN 27
+#define TX_PIN 26
 
+#define OUTPUT_PIN_1 22
+#define OUTPUT_PIN_2 23
+#define OUTPUT_PIN_3 -1
+#define OUTPUT_PIN_4 -1
+
+#define CRSF_MIN_VALUE 1000
+#define CRSF_MID_VALUE 1500
+#define CRSF_MAX_VALUE 2000
+
+#define LEFT_JOYSTICK_X 1
+#define LEFT_JOYSTICK_Y -1
+#define RIGHT_JOYSTICK_X -1
+#define RIGHT_JOYSTICK_Y 0
+
+#define BUTTON_B 6 
+
+#define MIN_FILTER 3
+
+#define FULL_SPEED_COEF 1.0
+#define LOW_SPEED_COEF 0.3
+
+int topRightWheelChannel = 1; //antenna tube is top left
+int botttomRightWheelChannel = 2;
+
+/*
+int topLeftWheelChannel = 1;
+int bottomLeftWheelChannel = 1;
+*/
+
+int y_cmd = 0;
+int x_cmd = 0;
+int b_cmd = 0;
+
+float speed_coef = FULL_SPEED_COEF;
 
 void SetServoPos(float percent, int pwmChannel)
 {
@@ -29,57 +56,195 @@ void SetServoPos(float percent, int pwmChannel)
     // 1/10th of 65536 = 6553.6
 
     uint32_t duty = map(percent, 0, 100, 3276.8, 6553.6);
-
     ledcWrite(pwmChannel, duty);
+    //Serial.print(pwmChannel);
+    //Serial.print(" ");
+    //Serial.println(duty);
+}
+
+int getBCmd() {
+  if (_raw_rc_values[BUTTON_B] != _old_rc_values[BUTTON_B]) {
+    return 1; //change speed mode
+  }
+  return 0;
+}
+
+int getXCmd() {
+  if (_raw_rc_values[LEFT_JOYSTICK_X] - _old_rc_values[LEFT_JOYSTICK_X] >= MIN_FILTER) {
+    return  1; //right turn
+  } else if (_raw_rc_values[LEFT_JOYSTICK_X] - _old_rc_values[LEFT_JOYSTICK_X] >= MIN_FILTER) {
+    return 2; //left turn
+  }
+  return 0;
+}
+
+int getYCmd() {
+  if (_raw_rc_values[RIGHT_JOYSTICK_Y] - _old_rc_values[RIGHT_JOYSTICK_Y] >= MIN_FILTER) {
+    //Serial.println("+");
+    return 1; // accelerate
+  } else if (_old_rc_values[RIGHT_JOYSTICK_Y] - _raw_rc_values[RIGHT_JOYSTICK_Y] >= MIN_FILTER) {
+    //Serial.println("-");
+    return 2;// break
+  } 
+  return y_cmd;
+}
+
+long move(char wheel, char direction, uint16_t val) {
+  if (wheel == 't' && direction == 'f') {
+    return map(val, CRSF_MAX_VALUE, CRSF_MIN_VALUE, 100, 0) * speed_coef;
+  } else if (wheel == 't' && direction == 'b') {
+    return map(val, CRSF_MIN_VALUE, CRSF_MAX_VALUE, 0, 100) * speed_coef;
+  } else if (wheel == 'b' && direction == 'f') {
+    return map(val, CRSF_MAX_VALUE, CRSF_MIN_VALUE, 0, 100) * speed_coef;    
+  } else if (wheel == 'b' && direction == 'b') {
+    return map(val, CRSF_MIN_VALUE, CRSF_MAX_VALUE, 100, 0) * speed_coef;
+  } else if (wheel == 't' && direction == 'n') {
+    return map(val, CRSF_MIN_VALUE, CRSF_MAX_VALUE, 0, 100) * speed_coef;
+  } else if (wheel == 'b' && direction == 'n') {
+    return map(val, CRSF_MIN_VALUE, CRSF_MAX_VALUE, 100, 0) * speed_coef;
+  }
 }
 
 void setup() {
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(460800);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(420000, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
   
-  ledcSetup(aileronsPWMChannel,50,16);
-  ledcSetup(elevatorPWMChannel,50,16);
-  ledcSetup(throttlePWMChannel,50,16);
-  ledcSetup(rudderPWMChannel,50,16);
+  Serial.begin(9600);
+  Serial2.begin(420000, SERIAL_8N1, RX_PIN, TX_PIN);
+  
+  ledcSetup(topRightWheelChannel,50,16);
+  ledcSetup(botttomRightWheelChannel,50,16);
+  /*
+  ledcSetup(topLeftWheelChannel,50,16);
+  ledcSetup(bottomLeftWheelChannel,50,16);
+  */
 
-  ledcAttachPin(aileronsPin, aileronsPWMChannel);
-  ledcAttachPin(elevatorPin, elevatorPWMChannel);
-  ledcAttachPin(throttlePin, throttlePWMChannel);
-  ledcAttachPin(rudderPin, rudderPWMChannel);
+
+  ledcAttachPin(OUTPUT_PIN_1, topRightWheelChannel);
+  ledcAttachPin(OUTPUT_PIN_2, botttomRightWheelChannel);
+  /*
+  ledcAttachPin(OUTPUT_PIN_3, topLeftWheelChannel);
+  ledcAttachPin(OUTPUT_PIN_4, bottomLeftWheelChannel);
+  */
 }
 
+
 void loop() { //Choose Serial1 or Serial2 as required
-  // Serial.println("looping");
+  
   while (Serial2.available()) {
     size_t numBytesRead = Serial2.readBytes(_rcs_buf, SBUS_BUFFER_SIZE);
     if(numBytesRead > 0)
     {
+      memcpy(_old_rc_values, _raw_rc_values, sizeof(_raw_rc_values));
       crsf_parse(&_rcs_buf[0], SBUS_BUFFER_SIZE, &_raw_rc_values[0], &_raw_rc_count, RC_INPUT_MAX_CHANNELS );
-      Serial.print("Channel 1: ");
+      /*Serial.print("Ch 1: ");
       Serial.print(_raw_rc_values[0]);
-      Serial.print("\tChannel 2: ");
+      Serial.print("\tCh 2: ");
       Serial.print(_raw_rc_values[1]);
-      Serial.print("\tChannel 3: ");
+      Serial.print("\tCh 3: ");
       Serial.print(_raw_rc_values[2]);
-      Serial.print("\tChannel 4: ");
+      Serial.print("\tCh 4: ");
       Serial.print(_raw_rc_values[3]);
-      Serial.print("\tChannel 5: ");
-      Serial.println(_raw_rc_values[4]);
+      Serial.print("\tCh 5: ");
+      Serial.println(_raw_rc_values[4]); */
 
-      int aileronsMapped = map(_raw_rc_values[0], 1000, 2000, 0, 100);
-      int elevatorMapped = map(_raw_rc_values[1], 1000, 2000, 0, 100);
-      int throttleMapped = map(_raw_rc_values[2], 1000, 2000, 0, 100);
-      int rudderMapped = map(_raw_rc_values[3], 1000, 2000, 0, 100);
-      int switchMapped = map(_raw_rc_values[4], 1000, 2000, 0, 100);
+      b_cmd = getBCmd();
+      y_cmd = getYCmd();
+      x_cmd = getXCmd();
+      
+      int topRightWheelMapped;
+      int bottomRightWheelMapped;
+      /*
+      int topLeftWheelMapped;
+      int bottomLeftWheelMapped;
+      */
+     
+      switch (b_cmd) {
+        case 1:
+          speed_coef == FULL_SPEED_COEF?speed_coef = LOW_SPEED_COEF:speed_coef = FULL_SPEED_COEF;
+          break;
+        default:
+          break;        
+      }
 
-      SetServoPos(aileronsMapped, aileronsPWMChannel);
-      SetServoPos(elevatorMapped, elevatorPWMChannel);
-      SetServoPos(throttleMapped, throttlePWMChannel);
-      SetServoPos(rudderMapped, rudderPWMChannel);
+      char direction = 'f';
+      if (y_cmd == 2) direction = 'b';
+      switch (x_cmd)
+      {
+        case 1:
+            topRightWheelMapped = move('t', direction, _raw_rc_values[LEFT_JOYSTICK_X]);
+            /*
+            bottomLeftWheelMapped = move('b', direction _raw_rc_values[LEFT_JOYSTICK_X]);
+            */
+
+          SetServoPos(topRightWheelMapped, topRightWheelChannel);
+          /*
+          SetServoPos(bottomLeftWheelMapped, botttomLeftWheelChannel);
+          */
+
+        case 2:
+          /*
+          topLefttWheelMapped = move('t', direction, _raw_rc_values[LEFT_JOYSTICK_X]);;
+          */
+          bottomRightWheelMapped = move('b', direction, _raw_rc_values[LEFT_JOYSTICK_X]);
+          /*
+          SetServoPos(topLeftWheelMapped, topLeftWheelChannel);
+          */
+          SetServoPos(bottomRightWheelMapped, botttomRightWheelChannel);
+        
+        default:
+          break;
+      }
+
+      switch (y_cmd)
+      {
+        case 1:
+          topRightWheelMapped = move('t', 'f', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          bottomRightWheelMapped = move('b', 'f', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          /*
+          topLeftWheelMapped = move('t', 'f', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          bottomLeftWheelMapped = move('t', 'f', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          */
+
+          SetServoPos(topRightWheelMapped, topRightWheelChannel);
+          SetServoPos(bottomRightWheelMapped, botttomRightWheelChannel);
+
+          /*
+          SetServoPos(topLeftWheelMapped, topLeftWheelChannel);
+          SetServoPos(bottomLeftWheelMapped, bottomLeftWheelChannel);
+          */
+          break;
+
+        case 2:
+          topRightWheelMapped = move('t', 'b', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          bottomRightWheelMapped = move('b', 'b', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          /*
+          topLeftWheelMapped = move('t', 'b', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          bottomLeftWheelMapped = move('b', 'b', _raw_rc_values[RIGHT_JOYSTICK_Y]);
+          */
+
+          SetServoPos(topRightWheelMapped, topRightWheelChannel);
+          SetServoPos(bottomRightWheelMapped, botttomRightWheelChannel);
+          /*
+          SetServoPos(topLeftWheelMapped, topLeftWheelChannel);
+          SetServoPos(bottomLeftWheelMapped, bottomLeftWheelChannel);          
+          */
+          break;
+
+        default:
+          topRightWheelMapped = move('t', 'n', CRSF_MID_VALUE);
+          bottomRightWheelMapped = move('b', 'n', CRSF_MID_VALUE);
+          /*
+          topLeftWheelMapped = move('t', 'n', CRSF_MID_VALUE);
+          bottomLeftWheelMapped = move('b', 'n', CRSF_MID_VALUE);
+          */
+
+          SetServoPos(topRightWheelMapped, topRightWheelChannel);
+          SetServoPos(bottomRightWheelMapped, botttomRightWheelChannel);
+          /*
+          SetServoPos(topLeftWheelMapped, topLeftWheelChannel);
+          SetServoPos(bottomLeftWheelMapped, bottomLeftWheelChannel)
+          */
+      }
     }
   }
 }
